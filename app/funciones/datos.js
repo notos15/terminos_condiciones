ac338bb526a7522e27c502dad5013632e9182fa0
notos15 <javier.martinez02@alumnos.uach.cl>
2024-10-12 00:46:45 -0300

    listo

--- app/funciones/datos.js
+++ app/funciones/datos.js
@@ -3,54 +3,52 @@ import formulario from "@/app/formulario/donador.json"
 import Cuadro from "@/app/funciones/ctexto.js"
 import Bot from "@/app/funciones/checkbox.js"
 import Libro from "@/app/formulario/libro.json"
-export function persona(){
-    return(
-        <div>
-            {formulario.cuadrotx.map((preg,i)=>(
-                <div className='rellenar'>
-                    <div className='tex'>{preg}</div>
-                    <Cuadro></Cuadro>
-                </div>
-            ))}
-     
-            {formulario.tes.map((pregunta,i)=>(
-                <div className='precionar' >
-                    <div className="titulo1">{pregunta.id}</div>
-                    <div className='rellenar'>
-                        {formulario.tes[i].title.map((title)=>(
-                            <div>
-                                <Bot></Bot>
-                                {title}
-                            </div>
-                                 
-                        ))}
-                    </div>
-                </div>    
-            ))}
-        </div>
-    )
-}
+export function persona() {
+    return (
+      <div>
+        {formulario.tes.map((pregunta, i) => (
+          <div className='precionar' key={i}>
+            <div className="titulo1">{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
+            <div className='rellenar'>
+              {pregunta.opciones.map((opcion, j) => (
+                <label key={j}>
+                  <input type={pregunta.tipo} name={pregunta.id} value={opcion} />
+                  {opcion}
+                </label>
+              ))}
+            </div>
+          </div>
+        ))}
+        {formulario.cuadrotx.map((preg) => (
+          <div className='rellenar' >
+            <div className='tex'>{preg.id}</div>
+            <Cuadro dato={preg.gris}></Cuadro>
+          </div>
+        ))}
+      </div>
+    );
+  }
 export function libro(){
     return(
         <div>
             {Libro.cuadrotx.map((preg)=>(
             <div className='rellenar'>
-                <div className='tex'>{preg}</div>
-                <Cuadro></Cuadro>
+                <div className='tex'>{preg.id}</div>
+                <Cuadro dato={preg.gris}></Cuadro>
             </div>
             ))}
-            {Libro.tes.map((pregunta,i)=>(
-                <div className='precionar'>
-                    <div className="titulo1">{pregunta.id}</div>
+            {Libro.tes.map((pregunta, i) => (
+                <div className='precionar' key={i}>
+                    <div className="titulo1">{pregunta.pregunta}</div> {/* Mostrar la pregunta */}
                     <div className='rellenar'>
-                        {Libro.tes[i].title.map((title)=>(
-                            <div>
-                                <Bot></Bot>
-                                {title}
-                            </div>     
+                        {pregunta.opciones.map((opcion, j) => (
+                            <label key={j}>
+                                <input type={pregunta.tipo} name={pregunta.id} value={opcion} />
+                                {opcion}
+                            </label>
                         ))}
                     </div>
-                </div>   
+                </div>
             ))}
         </div>
     )
--- app/page.js
+++ app/page.js
@@ -1,26 +1,45 @@
 "use client";
-import {  persona,libro } from "./funciones/datos.js";
+import {persona,libro } from "./funciones/datos.js";
+import Modal from "./funciones/modal.js";
+import { useState } from "react"; 
 //Donar Libros
 export default function Home() {
+  const [isModalOpen, setModalOpen] = useState(false); 
+  const [hasSeenTerms, setHasSeenTerms] = useState(false); 
+  const [isChecked, setChecked] = useState(false); 
   
+  const toggleModal = () => {
+    setModalOpen(!isModalOpen);
+  };
+  const handleModalClose = () => {
+    setHasSeenTerms(true); // Marca que ha visto los términos
+    setModalOpen(false); // Cierra el modal
+  };
+
   return (
-  <div>
+  <div >
     <h1 className="titulo">Donar Libros</h1>
     <div className="cuestionario">
       <h1 className="titulo1">Informacion del Usuario</h1>
       <div>{persona()}</div>
     </div>
-
     <div className="cuestionario">
       <h1 className="titulo1">Informacion del Libro</h1>
       <div>{libro()}</div>
+      <div className='rellenar'>
+        <label >
+            <input type="checkbox" checked={isChecked} onChange={(e) => setChecked(e.target.checked)} disabled={!hasSeenTerms}  />
+            <button className="texto-enlace"  onClick={toggleModal} >Terminos y condiciones</button>
+        </label>
         
+      </div>
     </div>
 
     <div className='footer'>
       <button className='btn'>Atras</button>
       <button className='btn'>Guardar</button>
     </div>
+    <Modal isOpen={isModalOpen} onClose={handleModalClose} />
   </div>
   )
 }
